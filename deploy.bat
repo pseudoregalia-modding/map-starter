8ecd1f1617e3f636acdd3a7a512bffbb8ec555b9
turncoda <144861042+turncoda@users.noreply.github.com>
2024-07-03 10:21:18 -0700

    Automate current path in deploy script

--- deploy.bat
+++ deploy.bat
@@ -3,9 +3,6 @@
 REM Output pak filename.
 set pak_filename=my_level_p
 
-REM Set this to be the folder containing the .uproject file.
-set uproject_folder=C:\Users\A\Documents\Unreal Projects\map-starter
-
 REM Point this to your Unreal Engine installation. (You probably don't have to modify this.)
 set run_uat=C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat
 
@@ -13,10 +10,10 @@ REM Point this to your Pseudoregalia installation. (You probably don't have to m
 set pseudoregalia_folder=C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia
 
 REM Cook and pak.
-call "%run_uat%" BuildCookRun -project="%uproject_folder%\pseudoregalia.uproject" -platform=Win64 -cook -stage -pak
+call "%run_uat%" BuildCookRun -project="%~dp0\pseudoregalia.uproject" -platform=Win64 -cook -stage -pak
 
 echo Copying pakchunk1-Windows.pak to Pseudoregalia folder, renamed as %pak_filename%.pak
-copy "%uproject_folder%\Saved\StagedBuilds\Windows\pseudoregalia\Content\Paks\pakchunk1-Windows.pak" "%pseudoregalia_folder%\pseudoregalia\Content\Paks\%pak_filename%.pak"
+copy "%~dp0\Saved\StagedBuilds\Windows\pseudoregalia\Content\Paks\pakchunk1-Windows.pak" "%pseudoregalia_folder%\pseudoregalia\Content\Paks\%pak_filename%.pak"
 
 echo Done!
 pause
